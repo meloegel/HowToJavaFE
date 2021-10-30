e068eeca6c0ab99608a4e2d4feede81c53467f66
meloegel <meloegel@gmail.com>
2021-10-30 19:37:08 -0400

    building basic login and added login schema

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,11 +1,68 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
+const initialFormValues = {
+    username: "",
+    password: ""
+}
+
+const initialFormErrors = {
+    username: "",
+    password: ""
+}
+
+const initalDisabled = true;
 
 export default function Login() {
+const [formValues, setFormValues] = useState(initialFormValues);
+const [formErrors, setFormErrors] = useState(initialFormErrors);
+const [disabled, setDisabled] = useState(initalDisabled);
+
+
+const onInputChange = (evt: any) => {
+    const name = evt.target.name;
+    const value = evt.target.value;
+
+    setFormValues({
+        ...formValues,
+        [name]:value
+    })
+}
+
+useEffect(() => {
+    
+})
 
     return (
         <div>
-            
+            <form>
+                <h2>Login</h2>
+                <input 
+                    value={formValues.username}
+                    onChange={onInputChange}
+                    name="username"
+                    type="text"
+                />
+                <input 
+                    value={formValues.password}
+                    onChange={onInputChange}
+                    name="password"
+                    type="text"
+                />
+                <div>
+                    <div>{formErrors.username}</div>
+                    <div>{formErrors.password}</div>
+                </div>
+                <div>
+                    <div>
+                        <h3>Register</h3>
+                        <button onClick={() => {}}>Register</button>
+                    </div>
+                    <div>
+                        <h3>Login</h3>
+                        <button onClick={() => {}}>Login</button>
+                    </div>
+                </div>
+            </form>
         </div>
     )
 }
