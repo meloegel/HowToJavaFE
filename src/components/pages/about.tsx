10d308211ee3ae592790b051c174ca647405a690
meloegel <meloegel@gmail.com>
2021-11-30 22:36:57 -0500

    added basic about

--- src/components/pages/about.tsx
+++ src/components/pages/about.tsx
@@ -7,8 +7,11 @@ export default function About(): JSX.Element {
       <Header showUser={true} />
       <div className="grid gird-cols-4 gap-6 p-4">
         <NavBar active={"about"} />
-        <div className="col-start-2 col-span-3 m-auto">
+        <div className="col-start-2 col-span-3 m-auto text-center bg-pink-300 p-8">
           <h2 className="text-center text-4xl p-4 mb-2">About</h2>
+          <p className="p-2">This project was my first project built using Java and Typescript.</p>
+          <p className="p-2">It stinks, but works.</p>
+          <a href="https://github.com/meloegel"><span className="underline">Github</span></a>
         </div>
       </div>
     </div>
