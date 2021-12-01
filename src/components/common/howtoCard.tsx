bb14c91f2856b6e00ce15a86a90f706a7d55e376
meloegel <meloegel@gmail.com>
2021-11-30 19:25:46 -0500

    baisc howto styling

--- src/components/common/howtoCard.tsx
+++ src/components/common/howtoCard.tsx
@@ -2,48 +2,68 @@ import { useEffect, useState } from "react";
 import useFetch from "../../hooks/useFetch";
 
 export type HowToCardType = {
-    howtoid: number;
-    name: string;
-    description: string;
-    category: string;
-    complexity: string;
-    user: string;
-}
+  howtoid: number;
+  name: string;
+  description: string;
+  category: string;
+  complexity: string;
+  user: string;
+};
 
-export default function HowToCard({name, description, category,complexity,user, howtoid}:HowToCardType):JSX.Element {
-    const [request, data] = useFetch<any>();
-    const [steps, setSteps] = useState([] as any[])
-    const token = window.localStorage.getItem("token");
+export default function HowToCard({
+  name,
+  description,
+  category,
+  complexity,
+  user,
+  howtoid,
+}: HowToCardType): JSX.Element {
+  const [request, data] = useFetch<any>();
+  const [steps, setSteps] = useState([] as any[]);
+  const token = window.localStorage.getItem("token");
 
-    useEffect(() => {
-        const headers = {
-          "Content-Type": "application/json",
-          Authorization: token!,
-        };
-        request(`http://localhost:2019/steps/steps/${howtoid}`, {
-          method: "GET",
-          headers: headers,
-        });
-      }, [request, token, howtoid]);
+  useEffect(() => {
+    const headers = {
+      "Content-Type": "application/json",
+      Authorization: token!,
+    };
+    request(`http://localhost:2019/steps/steps/${howtoid}`, {
+      method: "GET",
+      headers: headers,
+    });
+  }, [request, token, howtoid]);
 
-      useEffect(() => {
-        console.log(data);
-        if (data) {
-         setSteps(data)
-        }
-      }, [data]);
+  useEffect(() => {
+    console.log(data);
+    if (data) {
+      setSteps(data);
+    }
+  }, [data]);
 
-
-    return (
-        <div className="p-4 border m-2 w-full text-xl">
-            <h3>Name: {name}</h3>
-            <p>Description: {description}</p>
-            <p>Category: {category}</p>
-            {(complexity !== null) ? <p>Complexity: {complexity}</p> : null}
-            <p>User: {user}</p>
-            {steps.map(step => (
-                <p >Step: {step.step}</p>
-            ))}
-        </div>
-    )
+  return (
+    <div className="p-8 border-2  m-auto mb-4 w-1/2 text-md bg-red-300 text-white font-medium">
+      <h3 className="p-2 no-underline">
+        Name: <span className="text-2xl underline">{name}</span>
+      </h3>
+      <p className="p-2 no-underline">
+        Description: <span className="text-2xl underline">{description}</span>
+      </p>
+      <p className="p-2 no-underline">
+        Category: <span className="text-2xl underline">{category}</span>
+      </p>
+      {complexity !== null ? (
+        <p className="p-2 no-underline">
+          Complexity: <span className="text-2xl underline">{complexity}</span>
+        </p>
+      ) : null}
+      <p className="p-2 no-underline">
+        User: <span className="text-2xl underline">{user}</span>
+      </p>
+      {steps.map((step) => (
+        <p className="p-2 no-underline">
+          Step: <span className="text-2xl underline">{step.step}</span>
+        </p>
+      ))}
+    </div>
+  );
 }
--- src/components/pages/homePage.tsx
+++ src/components/pages/homePage.tsx
@@ -33,7 +33,7 @@ export default function HomePage() {
       <Header showUser={true} />
       <div className="grid gird-cols-4 gap-6 p-4">
         <NavBar active={"howtos"} />
-        <div className="col-start-2 col-span-3 m-auto">
+        <div className="col-start-2 col-span-3 m-auto w-full">
           <h2 className="text-center text-4xl p-4 mb-2">Howtos</h2>
           {howtos !== []
             ? howtos.map((howto): any => (
